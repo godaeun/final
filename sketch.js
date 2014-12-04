7674a78e81c504f1c722e6cd9052f8c0770aeac9
godaeun <gowooda@naver.com>
2014-12-04 23:48:32 +0900

    df

--- sketch.js
+++ sketch.js
@@ -29,20 +29,7 @@ function draw(){
     var time = (new Date())%1000/1000;
 
    
-    var time = (new Date())%1000/1000;
-    fill(0);
-   	ellipse(sin(time*2*PI)*100+bubbleX, bubbleY,bubbleY/3,bubbleY/3);
-   	bubbleY = bubbleY - 10;
-   	if(bubbleY < 0){
-   		bubbleY = 800;
-   	}
 
-    fill(0);
-   	ellipse(sin(time*2*PI)*50+bubbleX1, bubbleY1,bubbleY1/2,bubbleY1/2);
-   	bubbleY1 = bubbleY1 - 10;
-   	if(bubbleY1 < 0){
-   		bubbleY1 = 500;
-   	}
 
 
 
