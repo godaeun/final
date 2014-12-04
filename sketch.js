25391c6e9a86c0f9a8ead3a847ecafa05580c92c
godaeun <gowooda@naver.com>
2014-12-04 21:42:49 +0900

    fdd
    df

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
 
 var bg;
+var song, analyzer;
+
+function preload() {
+  song = loadSound("./croc.mp3");
 
 function setup(){
    bg = loadImage("1.jpg");
@@ -9,4 +13,9 @@ function draw(){
    background(bg);  
    createCanvas(1016,721);
 
+}
+
+
+function preload() {
+  song = loadSound("./cheese.mp3");
 }
