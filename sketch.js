c5de033ea6e13cad3d0864f0ca34231ceffb8e58
godaeun <gowooda@naver.com>
2014-12-04 15:13:17 +0900

    bh

--- sketch.js
+++ sketch.js
@@ -1,17 +1,17 @@
-var bg;
+function setup() {
+  createCanvas(1016, 721);
+  img = loadImage("1.png");
 
 
-function setup()
-{
-
-    createCanvas(1016,721);
-    bg = loadImage("bg.png");
 }
 
-function draw()
-{
-    background(bg);
-    fill(237,112,50);
-    noStroke();
-    
-    }
+function draw() {
+   background(img);
+
+
+   noStroke()
+   image(img, 0,500,270,270);
+
+
+}
+
