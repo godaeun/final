6b9a6c6b32c1aea568e4b77c3cf0a31bc77eee04
godaeun <gowooda@naver.com>
2014-12-06 14:21:48 +0900

    eef

--- sketch.js
+++ sketch.js
@@ -30,9 +30,8 @@ function draw(){
    var d2 = 10 + (sin(angle + PI/2) * diameter/2) + diameter/2;
    var d3 = 10 + (sin(angle + PI) * diameter/2) + diameter/2;
 
-   ellipse(0, height/2, d1, d1);
+
    ellipse(width/2, height/2, d2, d2);
-   ellipse(width, height/2, d3, d3);
 
    angle += 0.02;
 
