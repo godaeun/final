462aeaa90389a07c6700326c254bfcafd3dcbcc2
godaeun <gowooda@naver.com>
2014-12-06 16:38:43 +0900

    fsdf

--- sketch.js
+++ sketch.js
@@ -23,30 +23,6 @@ function setup(){
    img2 = loadImage("4.png");   
    img2 = loadImage("4.png");
 
-   tooth1 = loadImage("a.png");
-   tooth2 = loadImage("b.png");
-   tooth3 = loadImage("c.png");
-   tooth4 = loadimage("d.png");
-   tooth5 = loadImage("e.png");
-   tooth6 = loadImage("f.png");
-   tooth7 = loadImage("g.png");
-   tooth8 = loadimage("h.png");
-   tooth9 = loadImage("i.png");
-   tooth10 = loadImage("j.png");
-   tooth11 = loadImage("k.png");
-
-   pull1 = loadImage("aa.png");
-   pull2 = loadImage("bb.png");
-   pull3 = loadImage("cc.png");
-   pull4 = loadimage("dd.png");
-   pull5 = loadImage("ee.png");
-   pull6 = loadImage("ff.png");
-   pull7 = loadImage("gg.png");
-   pull8 = loadimage("hh.png");
-   pull9 = loadImage("ii.png");
-   pull10 = loadImage("jj.png");
-   pull11 = loadImage("kk.png");   
-
 
 }
 
