941e9d75e26146eae8fe1b481f01c7ea20b90f84
godaeun <gowooda@naver.com>
2014-12-06 14:28:36 +0900

    dsf

--- sketch.js
+++ sketch.js
@@ -24,14 +24,11 @@ function setup(){
 function draw(){
    background(bg); 
    image(img,218,61);
-   image(img1,580,150);   
+   image(img1,580,150, width/2, height/2, d2, d2);   
 
    var d1 = 10 + (sin(angle) * diameter/2) + diameter/2;
    var d2 = 10 + (sin(angle + PI/2) * diameter/2) + diameter/2;
    var d3 = 10 + (sin(angle + PI) * diameter/2) + diameter/2;
-
-
-   img1(width/2, height/2, d2, d2);
 
    angle += 0.02;
 
