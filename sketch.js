968ffcb6faa0edb343ee52c87bb259949a25c41e
godaeun <gowooda@naver.com>
2014-12-06 14:04:32 +0900

    ex

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 var bg;
 var soound, amp;
+var diameter; 
+var angle = 0;
 
 
 function preload() {
-  sound = loadSound("bgm.mp3"); 
+ sound = loadSound("bgm.mp3"); 
 }
 
 function setup(){
@@ -23,6 +25,15 @@ function draw(){
    image(img,218,61);
    image(img1,580,150);   
 
+   var d1 = 10 + (sin(angle) * diameter/2) + diameter/2;
+   var d2 = 10 + (sin(angle + PI/2) * diameter/2) + diameter/2;
+   var d3 = 10 + (sin(angle + PI) * diameter/2) + diameter/2;
+   
+   ellipse(0, height/2, d1, d1);
+   ellipse(width/2, height/2, d2, d2);
+   ellipse(width, height/2, d3, d3);
+   
+   angle += 0.02;
 
 }
 
