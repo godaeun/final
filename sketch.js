356ef413a7f58e44e1ca6f65ec6fde38a191842c
godaeun <gowooda@naver.com>
2014-12-06 17:58:22 +0900

    sdf

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var bg;
 var soound, amp;
 var diameter; 
 var angle = 0;
-var toothClicked =[];
+var toothClicked =[1,2,3,4,5,6,7,8,9,10,11];
 
 
 function preload() {
@@ -10,7 +10,7 @@ function preload() {
 }
 
 function setup(){
-   toothClicked[] = false;
+   toothClicked[]= false;
 
    createCanvas(1016,721);
    bg = loadImage("1.jpg");
@@ -45,29 +45,28 @@ function draw(){
    angle += 0.02;
 
 // a
-   for(var i = 0 ; i <12 ; i++){
-   if(toothClicked[i]){
+   if(toothClicked[1]){
    image(img15,340, 411);
    }else{
    image(img4,340, 383);   
    }
 
 // b
-   if(toothClicked[i]){
+   if(toothClicked[2]){
    image(img16,336, 464);
    }else{
    image(img5,336, 436);   
    }
-}
+
 
 }
-for(var i = 0 ; i <12 ; i++){
+
 function mousePressed(){
     if(mouseX > 340 && mouseX < 371 && mouseY > 383 && mouseY < 435){
         toothClicked[1] =true;
             }
 }
-}
+
 
 
 
