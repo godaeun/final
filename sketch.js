c479653a84b18b7f763a85b8febafcfdc38ab7e4
godaeun <gowooda@naver.com>
2014-12-06 15:02:46 +0900

    DA

--- sketch.js
+++ sketch.js
@@ -26,7 +26,7 @@ function draw(){
    image(img,218,61);
 
    var d2 = 10 + (sin(angle + PI/20) * diameter/20) + diameter/20;
-   image(img1, 590, 270, d2, d2);
+   image(img1, 580, 270, d2, d2);
    angle += 0.02;
 
 
