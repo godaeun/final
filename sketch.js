d82ea1eebbaecdeb2dfffe61b7d35ac58689ee02
godaeun <gowooda@naver.com>
2014-12-06 15:22:06 +0900

    sdf

--- sketch.js
+++ sketch.js
@@ -2,10 +2,11 @@ var bg;
 var soound, amp;
 var diameter; 
 var angle = 0;
+var input, button, greeting;
 
 
 function preload() {
- sound = loadSound("bgm.mp3"); 
+  sound = loadSound("bgm.mp3"); 
 }
 
 function setup(){
@@ -18,7 +19,20 @@ function setup(){
 
    img = loadImage("2.png");
    img1 = loadImage("3.png");
-   img2 = loadImage("4.png");   
+   img2 = loadImage("4.png");
+
+   input = createInput();
+   input.position(20, 65);
+
+   button = createButton('start');
+   button.position(150, 65);
+   button.mousePressed(greet);
+
+   greeting = createElement('h2', '악어의이빨을 뽑아주세요');
+   greeting.position(20, 5);
+
+   textAlign(CENTER)
+   textSize(50);   
 }
 
 function draw(){
@@ -30,6 +44,19 @@ function draw(){
    angle += 0.02;
 
 
+}
+
+function greet() {
+ var name = input.value();
+ greeting.html(''+name+'!');
+ input.value('');
+
+ for (var i=0; i<200; i++) {
+  push();
+  fill(random(255), 255, 255);
+  rect(50,50,50,50)
+  pop();
+}
 }
 
 
