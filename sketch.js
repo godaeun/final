1b7d1611723ab464276170f0522e71826bb28402
godaeun <gowooda@naver.com>
2014-12-06 16:49:37 +0900

    sdfsdf

--- sketch.js
+++ sketch.js
@@ -23,29 +23,29 @@ function setup(){
    img2 = loadImage("4.png");   
    img3 = loadImage("4.png");
 
-   img4 = loadImage("a.png");
-   img5 = loadImage("b.png");
-   img6 = loadImage("c.png");
-   img7 = loadimage("d.png");
-   img8 = loadImage("e.png");
-   img9 = loadImage("f.png");
-   img10 = loadImage("g.png");
-   img11 = loadimage("h.png");
-   img12 = loadImage("i.png");
-   img13 = loadImage("j.png");
-   img14 = loadImage("k.png");
+   img4 = loadImage("to1.png");
+   img5 = loadImage("to2.png");
+   img6 = loadImage("to3.png");
+   img7 = loadimage("to4.png");
+   img8 = loadImage("to5.png");
+   img9 = loadImage("to6.png");
+   img10 = loadImage("to7.png");
+   img11 = loadimage("to8.png");
+   img12 = loadImage("to9.png");
+   img13 = loadImage("to10.png");
+   img14 = loadImage("to11.png");
 
-   img15 = loadImage("aa.png");
-   img16 = loadImage("bb.png");
-   img17 = loadImage("cc.png");
-   img18 = loadimage("dd.png");
-   img19 = loadImage("ee.png");
-   img20 = loadImage("ff.png");
-   img21 = loadImage("gg.png");
-   img22 = loadimage("hh.png");
-   img23 = loadImage("ii.png");
-   img24 = loadImage("jj.png");
-   img25 = loadImage("kk.png");   
+   img15 = loadImage("t1.png");
+   img16 = loadImage("t2.png");
+   img17 = loadImage("t3.png");
+   img18 = loadimage("t4.png");
+   img19 = loadImage("t5.png");
+   img20 = loadImage("t6.png");
+   img21 = loadImage("t7.png");
+   img22 = loadimage("t8.png");
+   img23 = loadImage("t9.png");
+   img24 = loadImage("t10.png");
+   img25 = loadImage("t11.png");   
 
 
 }
