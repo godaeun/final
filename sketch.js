e9ca64a4b4c7c351ef11e2fab011adec277ddd57
godaeun <gowooda@naver.com>
2014-12-06 16:50:24 +0900

    dsf

--- sketch.js
+++ sketch.js
@@ -35,18 +35,6 @@ function setup(){
    img13 = loadImage("to10.png");
    img14 = loadImage("to11.png");
 
-   img15 = loadImage("t1.png");
-   img16 = loadImage("t2.png");
-   img17 = loadImage("t3.png");
-   img18 = loadimage("t4.png");
-   img19 = loadImage("t5.png");
-   img20 = loadImage("t6.png");
-   img21 = loadImage("t7.png");
-   img22 = loadimage("t8.png");
-   img23 = loadImage("t9.png");
-   img24 = loadImage("t10.png");
-   img25 = loadImage("t11.png");   
-
 
 }
 
