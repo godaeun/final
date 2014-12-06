676d8f6dcd42d8a917ed0589621b00e9e761c2d2
godaeun <gowooda@naver.com>
2014-12-06 18:33:19 +0900

    sdf

--- sketch.js
+++ sketch.js
@@ -2,33 +2,19 @@ var bg;
 var soound, amp;
 var diameter; 
 var angle = 0;
-var toothClicked1;
-var toothClicked2;
-var toothClicked3;
-var toothClicked4;
-var toothClicked5;
-var toothClicked6;
-var toothClicked7;
-var toothClicked8;
-var toothClicked9;
-var toothClicked10;
-var toothClicked11;
-
+var toothClicked;
 
 function preload() {
  sound = loadSound("bgm.mp3"); 
 }
 
 function setup(){
-   toothClicked= false;
-
+   toothClicked = false;
    createCanvas(1016,721);
    bg = loadImage("1.jpg");
-
    amp = new p5.Amplitude();
    amp.setInput(sound);
    sound.loop();
-
    diameter = height - 40;
 
 
@@ -39,36 +25,6 @@ function setup(){
 
    img4 = loadImage("to1.png");
    img15 = loadImage("t1.png");
-
-   img5 = loadImage("to2.png");
-   img16 = loadImage("t2.png");
-
-   img6 = loadImage("to3.png");
-   img17 = loadImage("t3.png");
-
-   img7 = loadimage("to4.png");
-   img18 = loadimage("t4.png");   
-
-   img8 = loadImage("to5.png");
-   img19 = loadImage("t5.png");   
-
-   img9 = loadImage("to6.png");
-   img20 = loadImage("t6.png");   
-
-   img10 = loadImage("to7.png");
-   img21 = loadImage("t7.png");   
-
-   img11 = loadimage("to8.png");
-   img22 = loadimage("t8.png");   
-
-   img12 = loadImage("to9.png");
-   img23 = loadImage("t9.png");
-
-   img13 = loadImage("to10.png");
-   img24 = loadImage("t10.png");
-
-   img14 = loadImage("to11.png");
-   img25 = loadImage("t11.png");  
 
 
 }
@@ -82,109 +38,22 @@ function draw(){
    angle += 0.02;
 
 
-   if(toothClicked1){
+   if(toothClicked){
    image(img15,340, 411);
    }else{
    image(img4,340, 383);   
    }
 
-   if(toothClicked2){
-   image(img16,336, 464);
-   }else{
-   image(img5,336, 436);   
-   }
 
-   if(toothClicked3){
-   image(img17,357, 501);
-   }else{
-   image(img6,357, 469);   
-   }
 
-   if(toothClicked4){
-   image(img18,393, 523);
-   }else{
-   image(img7,393, 493);   
-   }
-
-   if(toothClicked5){
-   image(img19,440, 537);
-   }else{
-   image(img8,440, 505);   
-   }      
-   
-   if(toothClicked6){
-   image(img20,491, 537);
-   }else{
-   image(img9,491, 508);   
-   }
-
-   if(toothClicked7){
-   image(img21,548, 536);
-   }else{
-   image(img10,548, 504);   
-   }
-
-   if(toothClicked8){
-   image(img22,596, 523);
-   }else{
-   image(img11,596, 492);   
-   }
-
-   if(toothClicked9){
-   image(img23,637, 501);
-   }else{
-   image(img12,637, 469);   
-   }      
-   
-   if(toothClicked10){
-   image(img24,669, 464);
-   }else{
-   image(img13,669, 436);   
-   }   
-
-   if(toothClicked11){
-   image(img25,665, 410);
-   }else{
-   image(img14,665, 383);   
-   }     
 }
 
 function mousePressed(){
     if(mouseX > 340 && mouseX < 371 && mouseY > 383 && mouseY < 435){
-        toothClicked1 =true;
-            }
-    if(mouseX > 336 && mouseX < 365 && mouseY > 436 && mouseY < 486){
-        toothClicked2 =true;
-            }
-    if(mouseX > 357 && mouseX < 394 && mouseY > 469 && mouseY < 528){
-        toothClicked3 =true;
-            }
-    if(mouseX > 393 && mouseX < 436 && mouseY > 493 && mouseY < 549){
-        toothClicked4 =true;
-            }
-    if(mouseX > 440 && mouseX < 486 && mouseY > 505 && mouseY < 562){
-        toothClicked5 =true;
-            }
-    if(mouseX > 491 && mouseX < 541 && mouseY > 508 && mouseY < 564){
-        toothClicked6 =true;
-            }
-    if(mouseX > 548 && mouseX < 594 && mouseY > 504 && mouseY < 560){
-        toothClicked7 =true;
-            }
-    if(mouseX > 596 && mouseX < 639 && mouseY > 492 && mouseY < 549){
-        toothClicked8 =true;
-            }
-    if(mouseX > 637 && mouseX < 674 && mouseY > 469 && mouseY < 528){
-        toothClicked9 =true;
-            }
-    if(mouseX > 669 && mouseX < 698 && mouseY > 436 && mouseY < 486){
-        toothClicked10 =true;
-            }                                                
-    if(mouseX > 665 && mouseX < 696 && mouseY > 383 && mouseY < 434){
-        toothClicked11 =true;
-            }
+        toothClicked =true;
+    }
 }
 
-
-
-
+    if(mouseX > 336 && mouseX < 365 && mouseY > 436 && mouseY < 486){
+        toothClicked =true;
+            }
