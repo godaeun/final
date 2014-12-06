f2f920d1955fa54c4593ed9619e34679aa61d35e
godaeun <gowooda@naver.com>
2014-12-06 15:24:56 +0900

    dfsd

--- sketch.js
+++ sketch.js
@@ -2,25 +2,10 @@ var bg;
 var soound, amp;
 var diameter; 
 var angle = 0;
-var input, button, greeting;
 
 
 function preload() {
-  sound = loadSound("bgm.mp3"); 
-}
-
-
-function greet() {
- var name = input.value();
- greeting.html(''+name+'!');
- input.value('');
-
- for (var i=0; i<200; i++) {
-  push();
-  fill(random(255), 255, 255);
-  rect(50,50,50,50)
-  pop();
-}
+ sound = loadSound("bgm.mp3"); 
 }
 
 function setup(){
@@ -29,24 +14,11 @@ function setup(){
    amp = new p5.Amplitude();
    amp.setInput(sound);
    sound.loop();
-   diameter = height - 30;
+   diameter = height - 40;
 
    img = loadImage("2.png");
    img1 = loadImage("3.png");
-   img2 = loadImage("4.png");
-
-   input = createInput();
-   input.position(20, 65);
-
-   button = createButton('start');
-   button.position(150, 65);
-   button.mousePressed(greet);
-
-   greeting = createElement('h2', '악어의이빨을 뽑아주세요');
-   greeting.position(20, 5);
-
-   textAlign(CENTER)
-   textSize(50);   
+   img2 = loadImage("4.png");   
 }
 
 function draw(){
@@ -59,7 +31,6 @@ function draw(){
 
 
 }
-
 
 
 
