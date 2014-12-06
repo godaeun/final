f9503105f31e072afb672c86dce04531431a325f
godaeun <gowooda@naver.com>
2014-12-06 16:33:32 +0900

    sdf

--- sketch.js
+++ sketch.js
@@ -17,43 +17,41 @@ function setup(){
    sound.loop();
    diameter = height - 40;
 
+
    img = loadImage("2.png");
    img1 = loadImage("3.png");
+   img2 = loadImage("4.png");   
    img2 = loadImage("4.png");
 
-   img3 = loadImage('a.png');
-   img4 = loadImage('b.png');
-   img5 = loadImage('c.png');
-   img6 = loadimage('d.png');
-   img7 = loadImage('e.png');
-   img8 = loadImage('f.png');
-   img9 = loadImage('g.png');
-   img10 = loadimage('h.png');
-   img11 = loadImage('i.png');
-   img12 = loadImage('j.png');
-   img13 = loadImage('k.png');
+   tooth1 = loadImage("a.png");
+   tooth2 = loadImage("b.png");
+   tooth3 = loadImage("c.png");
+   tooth4 = loadimage("d.png");
+   tooth1 = loadImage("e.png");
+   tooth2 = loadImage("f.png");
+   tooth3 = loadImage("g.png");
+   tooth4 = loadimage("h.png");
+   tooth1 = loadImage("i.png");
+   tooth2 = loadImage("j.png");
+   tooth3 = loadImage("k.png");
 
-   img14 = loadImage('aa.png');
-   img15 = loadImage('bb.png');
-   img16 = loadImage('cc.png');
-   img17 = loadimage('dd.png');
-   img18 = loadImage('ee.png');
-   img19 = loadImage('ff.png');
-   img20 = loadImage('gg.png');
-   img21 = loadimage('hh.png');
-   img22 = loadImage('ii.png');
-   img23 = loadImage('jj.png');
-   img24 = loadImage('kk.png');   
+   pull1 = loadImage("aa.png");
+   pull2 = loadImage("bb.png");
+   pull3 = loadImage("cc.png");
+   pull4 = loadimage("dd.png");
+   pull1 = loadImage("ee.png");
+   pull2 = loadImage("ff.png");
+   pull3 = loadImage("gg.png");
+   pull4 = loadimage("hh.png");
+   pull1 = loadImage("ii.png");
+   pull2 = loadImage("jj.png");
+   pull3 = loadImage("kk.png");   
 
 
 }
 
 function draw(){
-   background(0);   
-   background(bg); 
-   image(img,218,61);
-
-   var d2 = 10 + (sin(angle + PI/20) * diameter/20) + diameter/20;
+@@ -31,21 +57,19 @@ function draw(){
    image(img1, 580, 270, d2, d2);
    angle += 0.02;
 
@@ -61,18 +59,22 @@ function draw(){
    if(toothClicked){
         fill(0,255,0);
         rect(200,300,100,200);
+        image(tooth1, 340,383);
    }else{
         fill(0,255,0);
         rect(200,100,100,500);
+        image(pull1, 340,411);
    }
 
    fill(255);
    rect(300,150,50,120);
+   image(tooth1, 340,383);
 
 }
 
 function mousePressed(){
     if(mouseX > 300 && mouseX < 350 && mouseY > 150 && mouseY < 270){
+    if(mouseX > 340 && mouseX < 371 && mouseY > 383 && mouseY < 435){
         toothClicked =true;
     }
 }
